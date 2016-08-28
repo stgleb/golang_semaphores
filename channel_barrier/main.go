a3bd5bc192377ca6808320b24a5a0f82a65415ae
Gleb Stepanov <gstepanov@mirantis.com>
2016-08-28 15:09:50 +0300

    Const added.

--- channel_barrier/main.go
+++ channel_barrier/main.go
@@ -12,6 +12,8 @@ import (
 */
 
 
+const N = 10
+
 func foo(id int, ch chan int) {
 	fmt.Printf("Hello, i am %d\n",id)
 	ch <- id
@@ -21,20 +23,21 @@ func main() {
 	ch := make(chan int)
 	var ch2 chan int
 
-	for i:= 0; i < 10;i++ {
+	for i:= 0; i < N;i++ {
 		go foo(i, ch)
 	}
 
-	for i := 0;i < 5; i++ {
+	for i := 0;i < N/2; i++ {
 		<- ch
 	}
+
 	ch2 = ch
 	ch = nil
 	fmt.Println("Stop receiving and sleep")
 	time.Sleep(1)
 	ch = ch2
 
-	for i := 0;i < 5; i++ {
+	for i := 0;i < N/2; i++ {
 		<- ch
 	}
 }
